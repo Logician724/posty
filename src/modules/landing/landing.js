8e16d63ecc6f9b27b6b42e29355989412d92770d
Logician724 <reda.i@eseed.net>
2019-02-04 15:40:17 +0200

    feat: Add landing page

--- src/modules/landing/landing.js
+++ src/modules/landing/landing.js
@@ -1,7 +1,26 @@
-
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 const landing = () => (
-    <div>in landing</div>
+    <div className='w-100'>
+        <div className='half-page text-center d-flex w-100 p-3 mx-auto flex-column'>
+            <div className='mt-auto mb-auto center'>
+                <div className='landing-info'>
+                    <Typography component='h1' variant='h1'>
+                        Posty
+                    </Typography>
+                    <p > Something I hacked in a couple of hours</p>
+                    <Link to='/signup' className='no-decoration' >
+                        <Button>
+                            Sign Up Now
+                    </Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
 );
 
 export default landing;
