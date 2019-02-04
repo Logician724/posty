29b0bef33897abe10587a73cdc9b39fcfb3b817f
Logician724 <reda.i@eseed.net>
2019-02-04 03:37:27 +0200

    chore: Add placholders for the rest of the interfaces

---
+++ src/modules/landing/landing.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+const landing = () => (
+    <div>in landing</div>
+);
+
+export default landing;
+
---
+++ src/modules/posts/view/view.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const PostView = () => (<div>In posts view</div>);
+export default PostView;
